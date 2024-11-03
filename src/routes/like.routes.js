39349dd193be12b7dae12289cde0c3c88eff2be0
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-11-03 22:37:07 +0530

    likes toggles routes updated

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -6,12 +6,11 @@ import {
   toggleVideoLike,
 } from "../controllers/like.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
 const router = Router();
 const securedRoute = router.use(verifyJWT);
-securedRoute.route("toogle/v/:videoId").post(toggleVideoLike);
-securedRoute.route("toogle/c/:commentId").post(toggleCommentsLike);
-securedRoute.route("toogle/t/:tweetId").post(toggleTweetLike);
+securedRoute.route("/toggle/v/:videoId").post(toggleVideoLike);
+securedRoute.route("/toggle/c/:commentId").post(toggleCommentsLike);
+securedRoute.route("/toggle/t/:tweetId").post(toggleTweetLike);
 securedRoute.route("/videos").get(getLikedVideos);
 
 export default router;

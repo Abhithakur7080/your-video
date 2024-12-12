6858dacd34aa802446271dd3e1a6b239aeba304b
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-12-12 14:26:21 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(
 
 // App configuration
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev"));
